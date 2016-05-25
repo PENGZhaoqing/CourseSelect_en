58487bdd8d7f676342530029efd6dedc38e65872
peng <ppp8300885@gmail.com>
2016-05-25 11:41:40 +0800

    test3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # require 'doorkeeper'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :doorkeeper,"bcbd21d235f47141f54e63315a9581b854c308bd02b43a6f3b98d27673cf6928", "d62b46a649a6f0a89267bb2a65104a7d1af194f4b65e34210e46a242d5a0bafb"
+  provider :doorkeeper,"1359396eeda34a5dc25e472c6b8d5a53680f62053bb9296d36812a7a07d32c54", "074dd7debfe5ff2aafec081e2a9f38df5a890df82afa737ea209f250a5add29b"
 end
