dff4ea9e7defcdc9dcaf4206083b606f2263bc5e
peng <ppp8300885@gmail.com>
2016-09-27 23:36:30 +0800

    heroku final

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # require 'doorkeeper'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :doorkeeper,"3b6c4adda378463f79624afb72cf285d23de09a170247424e51ace0951166a6f", "d22c97a9df2ea3ae798cb3201f3a74806093b4c87a11cc940cc4b88e8898f78b"
+  provider :doorkeeper,"26322d2e8b1a9f1c4aa90e564c2d75b88284820189205c69e489c0bd56f3ab06", "4a95f5bc3abe35b39307cf8540bab66fe8cedad94f174ab6724fcfccac502bc4"
 end
