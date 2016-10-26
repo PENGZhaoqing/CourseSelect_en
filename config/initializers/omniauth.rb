3dfea1f7414d4b319e9915407011edb88c111c0f
peng <ppp8300885@gmail.com>
2016-10-26 18:52:19 +0800

    fix the login bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # require 'doorkeeper'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :doorkeeper,"26322d2e8b1a9f1c4aa90e564c2d75b88284820189205c69e489c0bd56f3ab06", "4a95f5bc3abe35b39307cf8540bab66fe8cedad94f174ab6724fcfccac502bc4"
+  provider :doorkeeper,"b90cd219e59d98ff056f1cafe8a1afab0c005e2c25acf4747930273762482586", "b88432728760ff5d129004cdcd9d0dd76ca7e3316ac7c98c6d97dca99ab022ab"
 end
